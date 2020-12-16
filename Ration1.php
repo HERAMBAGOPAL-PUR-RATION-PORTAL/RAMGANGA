579d867d6da613fd186e0528f25c1c93167c3943
হেরম্বগোপালপুর‌রেশন পোর্টাল <75358400+HERAMBAGOPAL-PUR-RATION-PORTAL@users.noreply.github.com>
2020-12-16 19:17:34 +0530

    Ration1.html
    Ration1

--- Ration1.php
+++ Ration1.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
+<title>স্বাগতম আপনাকে হেরম্বগোপালপুরের রেশন পোর্টাল এ !</title>
 <meta name="viewport" content="width=device width, initial-scale 1.0"/>
 <style>
 *{
